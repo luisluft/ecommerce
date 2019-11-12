308e7899c5cd0b184429f857ef91e86aac5147d9
luisluft <luberluft@gmail.com>
2019-11-12 15:01:51 -0400

    add page and tpl admin

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once("vendor/autoload.php");
 // Namespaces inside the vendor folder
 use Slim\Slim;
 use Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -14,6 +15,12 @@ $app->config('debug', true);
 // Redirects the site via GET at route '/'
 $app->get('/', function() {
 	$page = new Page(); // adds the header and footer
+	$page->setTpl("index"); // adds the index.html 
+});
+
+// Route direction for the admin page
+$app->get('/admin', function() {
+	$page = new PageAdmin(); // adds the header and footer
 	$page->setTpl("index"); // adds the index.html 
 });
 
