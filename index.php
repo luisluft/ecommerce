ed2b3805e47865c923366de30831c2e6e49753b9
luisluft <luberluft@gmail.com>
2019-11-12 11:56:22 -0400

    add class page and remove config files

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 // Composer's autoload for dependencies
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+// Namespaces inside the vendor folder
+use Slim\Slim;
+use Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
+// Redirects the site via GET at route '/'
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
-
+	$page = new Page(); // adds the header and footer
+	$page->setTpl("index"); // adds the index.html 
 });
 
 $app->run();
