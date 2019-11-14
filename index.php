df602e2fe618634d971e9dce56e578816e487f4e
luisluft <luberluft@gmail.com>
2019-11-14 14:27:29 -0400

    create, read, update and delete users

--- index.php
+++ index.php
@@ -65,4 +65,99 @@ $app->get(
     }
 );
 
+$app->get(
+    '/admin/users',
+    function () {
+        User::verifyLogin();
+
+        $users = User::listAll();
+
+        $page = new PageAdmin();
+
+        $page->setTpl("users", array(
+            "users"=>$users
+        ));
+    }
+);
+
+$app->get(
+    "/admin/users/create",
+    function () {
+        User::verifyLogin();
+        $page = new PageAdmin();
+        $page->setTpl("users-create");
+    }
+);
+
+$app->post(
+    "/admin/users/create",
+    function () {
+        User::verifyLogin();
+
+        $user = new User();
+
+        $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+        $user->setData($_POST);
+
+        $user->save();
+
+        header("Location: /admin/users");
+        exit;
+    }
+);
+
+// This route must be atop of the route  '.../:iduser' because SLIM will fail to understand it otherwise
+$app->get(
+    "/admin/users/:iduser/delete",
+    function ($iduser) {
+        User::verifyLogin();
+
+        $user = new User();
+        
+        $user->get((int)$iduser);
+
+        $user->delete();
+        
+        header("Location: /admin/users");
+
+        exit;
+    }
+);
+
+$app->get(
+    '/admin/users/:iduser', // In this case the parameter binding is automatic
+    function ($iduser) {
+        User::verifyLogin();
+        $user = new User();
+        $user->get((int)$iduser);
+        $page = new PageAdmin();
+        $page->setTpl("users-update", array(
+            "user"=>$user->getValues()
+        ));
+    }
+);
+
+
+$app->post(
+    "/admin/users/:iduser",
+    function ($iduser) {
+        User::verifyLogin();
+        
+        $user = new User();
+        
+        $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+        $user->get((int)$iduser);
+        
+        $user->setData($_POST);
+        
+        $user->update();
+
+        header("Location: /admin/users");
+        
+        exit;
+    }
+);
+
 $app->run();
