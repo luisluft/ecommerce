55ba832cc2c2b0ea79cb66049e9bfc352f968c12
luisluft <luberluft@gmail.com>
2019-11-18 15:11:17 -0400

    admin products

---
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -0,0 +1,124 @@
+<?php
+
+// Tells php the location of this file
+namespace Hcode\Model;
+
+// Tells php where the requested class is located from the root folder
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+use \Hcode\Mailer;
+
+class Product extends Model
+{
+    public static function listAll()
+    {
+        $sql = new Sql();
+        
+        return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
+    }
+
+    public function save()
+    {
+        $sql = new Sql();
+        
+        $results = $sql->select(
+            "CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)",
+            array(
+            ":idproduct"=>$this->getidproduct(),
+            ":desproduct"=>$this->getdesproduct(),
+            ":vlprice"=>$this->getvlprice(),
+            ":vlwidth"=>$this->getvlwidth(),
+            ":vlheight"=>$this->getvlheight(),
+            ":vllength"=>$this->getvllength(),
+            ":vlweight"=>$this->getvlweight(),
+            ":desurl"=>$this->getdesurl()
+             )
+        );
+
+        $this->setData($results[0]);
+    }
+
+    public function get($idproduct)
+    {
+        $sql = new Sql();
+        
+        $results = $sql->select("SELECT * FROM tb_products WHERE idproduct = :idproduct", array(
+            ":idproduct"=>$idproduct
+        ));
+
+        $this->setData($results[0]);
+    }
+
+    public function delete()
+    {
+        $sql = new Sql();
+
+        $sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct", [
+            ":idproduct"=>$this->getidproduct()
+        ]);
+    }
+
+
+    public function checkPhoto()
+    {
+        $fileName = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .
+            "resources" . DIRECTORY_SEPARATOR .
+            "site" . DIRECTORY_SEPARATOR .
+            "img" . DIRECTORY_SEPARATOR .
+            "products" . DIRECTORY_SEPARATOR .
+            $this->getidproduct() . ".jpg";
+        
+        // This code is url so you do not need the 'directory separator' constant
+        if (file_exists($fileName)) {
+            $url = "/resources/site/img/products/" . $this->getidproduct() . ".jpg";
+        } else {
+            $url = "/resources/site/img/product.jpg"; // default picture when there is none
+        }
+
+        $this->setdesphoto($url);
+    }
+
+    public function getValues()
+    {
+        $this->checkPhoto();
+
+        $values = parent::getValues();
+
+        return $values;
+    }
+
+    public function setPhoto($file)
+    {
+        $extension = explode('.', $file['name']);
+
+        $extension = end($extension);
+    
+        switch ($extension) {
+            case 'jpg':
+            case 'jpeg':
+                $image = imagecreatefromjpeg($file['tmp_name']);
+                break;
+            
+            case 'gif':
+                $image = imagecreatefromgif($file['tmp_name']);
+                break;
+            
+            case 'png':
+                $image = imagecreatefrompng($file['tmp_name']);
+                break;
+        }
+
+        $destination = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .
+        "resources" . DIRECTORY_SEPARATOR .
+        "site" . DIRECTORY_SEPARATOR .
+        "img" . DIRECTORY_SEPARATOR .
+        "products" . DIRECTORY_SEPARATOR .
+        $this->getidproduct() . ".jpg";
+
+        imagejpeg($image, $destination);
+
+        imagedestroy($image);
+
+        $this->checkPhoto();
+    }
+}
