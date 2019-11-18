9338e8644536a63b3dd33c99d8609f6587cc2753
luisluft <luberluft@gmail.com>
2019-11-18 17:56:13 -0400

    site products

--- site.php
+++ site.php
@@ -2,12 +2,18 @@
 
 use Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\Product;
 
 // Redirects the site via GET at route '/'
 $app->get(
     '/',
     function () {
+        $products = Product::listAll();
+
         $page = new Page(); // adds the header and footer
-        $page->setTpl("index"); // adds the index.html
+
+        $page->setTpl("index", [
+            'products'=>Product::checkList($products)
+        ]); // adds the index.html
     }
 );
