28be81d65be7d52fdd09f083b13401b18e98b309
luisluft <luberluft@gmail.com>
2019-11-20 21:52:07 -0400

    login site

---
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -0,0 +1,12 @@
+<?php
+
+// Tells php the location of this file
+namespace Hcode\Model;
+
+// Tells php where the requested class is located from the root folder
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class Address extends Model
+{
+}
