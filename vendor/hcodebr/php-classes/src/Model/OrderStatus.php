0fab6a07db38ad9d2e885beaf270820419b42d04
luisluft <luberluft@gmail.com>
2019-11-22 20:30:26 -0400

    payment slip and close order

---
+++ vendor/hcodebr/php-classes/src/Model/OrderStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+// Tells php the location of this file
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class OrderStatus extends Model
+{
+    const EM_ABERTO = 1;
+    const AGUARDANDO_PAGAMENTO = 2;
+    const PAGO = 3;
+    const ENTREGUE = 4;
+}
