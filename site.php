bfd83ae3d2fb7f8034cc08f594a2be884061b899
luisluft <luberluft@gmail.com>
2019-12-02 22:20:36 -0400

    Pagseguro Integration

--- site.php
+++ site.php
@@ -548,8 +548,44 @@ $app->post(
 
         $order->save();
 
-        header('Location: /order/' . $order->getidorder());
+        header('Location: /order/' . $order->getidorder() . "/pagseguro");
         exit;
+    }
+);
+
+
+$app->get(
+    "/order/:idorder/pagseguro",
+    function ($idorder) {
+        User::verifyLogin(false);
+
+        $order = new Order();
+        
+        $order->get((int)$idorder);
+
+        $cart = $order->getCart();
+
+        $page = new Page(
+            [
+                'header'=>false,
+                'footer'=>false
+            ]
+        );
+        
+        $page->setTpl(
+            "payment-pagseguro",
+            [
+                'order'=>$order->getValues(),
+                'cart'=>$cart->getValues(),
+                'email'=>Mailer::readConfigFile('pagseguro'),
+                'products'=>$cart->getProducts(),
+                'phone'=>
+                [
+                    'areaCode'=>substr($order->getnrphone(), 0, 2),
+                    'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
+                ]
+            ]
+        );
     }
 );
 
