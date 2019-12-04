c3e921756501629e0621961153af448da6aa92cc
luisluft <luberluft@gmail.com>
2019-12-04 09:42:41 -0400

    phpinfo page

--- admin.php
+++ admin.php
@@ -3,6 +3,13 @@
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
+// Informs the php version
+$app->get(
+    '/phpinfo',
+    function () {
+        phpinfo();
+    }
+);
 
 // Route direction for the admin page
 $app->get(
@@ -19,8 +26,8 @@ $app->get(
     function () {
         $page = new PageAdmin(
             [
-            "header" => false,
-            "footer" => false
+                "header" => false,
+                "footer" => false
             ]
         );
         $page->setTpl("login");
@@ -50,8 +57,8 @@ $app->get(
     function () {
         $page = new PageAdmin(
             [
-            "header" => false,
-            "footer" => false
+                "header" => false,
+                "footer" => false
             ]
         );
 
@@ -75,8 +82,8 @@ $app->get(
     function () {
         $page = new PageAdmin(
             [
-            "header" => false,
-            "footer" => false
+                "header" => false,
+                "footer" => false
             ]
         );
 
@@ -88,17 +95,17 @@ $app->get(
     "/admin/forgot/reset",
     function () {
         $user = User::validForgotDecrypt($_GET["code"]);
-        
+
         $page = new PageAdmin(
             [
-            "header" => false,
-            "footer" => false
+                "header" => false,
+                "footer" => false
             ]
         );
 
         $page->setTpl("forgot-reset", array(
-            "name"=>$user["desperson"],
-            "code"=>$_GET["code"]
+            "name" => $user["desperson"],
+            "code" => $_GET["code"]
         ));
     }
 );
@@ -107,23 +114,23 @@ $app->post(
     "/admin/forgot/reset",
     function () {
         $forgot = User::validForgotDecrypt($_POST["code"]);
-    
+
         User::setForgotUsed($forgot["idrecovery"]);
 
         $user = new User();
 
-        $user->get((int)$forgot["iduser"]);
-        
+        $user->get((int) $forgot["iduser"]);
+
         $password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
-            "cost"=>12
+            "cost" => 12
         ]);
 
         $user->setPassword($password);
 
         $page = new PageAdmin(
             [
-            "header" => false,
-            "footer" => false
+                "header" => false,
+                "footer" => false
             ]
         );
 
